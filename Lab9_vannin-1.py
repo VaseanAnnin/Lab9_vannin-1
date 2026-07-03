9049957f04ab4f48d2135dc6e1257b07e257e6ab
Vasean Annin <vaseanannin@gmail.com>
2026-07-03 04:22:48 -0400

    init

---
+++ Lab9_vannin-1.py
@@ -0,0 +1,10 @@
+"""
+Name: LAB 9 Match Coins
+Author: Vasean Annin
+Purpose: This program runs a game with another player 
+the players toss a coin and lose or gain money depending on who wins
+the game ends when a player has 0 coins or stops the game 
+Date: 07/03/2026
+
+"""
+
